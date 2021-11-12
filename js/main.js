f3bc00ace8b96c025358128cfec409cae47db193
Svdex <ucihaibna@yahoo.com>
2021-11-13 05:56:48 +0700

    fix spine path

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ function loadChar(model="Shiroko_home/Shiroko_home.skel") {
 
     // load new spine
     app.loader
-        .add('char', `../assets/${model}`)
+        .add('char', `./assets/${model}`)
         .load(onAssetsLoaded);
 }
 
