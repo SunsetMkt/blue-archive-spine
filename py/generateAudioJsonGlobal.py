51760b94a3c1f87d25a09f3f82c47e32b9be9b67
Sunset <26019675+lwd-temp@users.noreply.github.com>
2022-09-16 00:46:56 +0800

    Japan

---
+++ py/generateAudioJsonGlobal.py
@@ -0,0 +1,48 @@
+import os
+import json
+import requests
+
+from getModelsGlobal import getResourceURL, downloadFile
+
+data = {}
+
+# 1 for offline, 0 for online but cors issue
+_type = 1
+
+option = {
+    "skipExisting": True
+}
+
+if not(os.path.isdir("./data")):
+    os.mkdir("./data")
+
+if __name__ == "__main__":
+    resUrl = getResourceURL()
+    baseUrl = '/'.join(resUrl.split("/")[0:-1])
+    res = requests.get(resUrl).json()["resources"]
+    for asset in res:
+        if "Audio/VOC_JP/" in asset["resource_path"]:
+            keyEvent = ''.join(asset["resource_path"].split("/")[-1].split(".")[:-1])
+            fname = ''.join(asset["resource_path"].split("/")[-1])
+            
+            # download ver
+            if _type:
+                path = f"./assets/audio/{fname}"
+                print("="*30)
+                print(fname)
+                if os.path.isfile(path):
+                    print("Already downloaded. Skipping.")
+                    data[keyEvent] = path
+                    continue
+                if not(os.path.isdir("./assets/audio")):
+                    os.mkdir("./assets/audio/")
+                downloadFile(baseUrl + "/" + asset["resource_path"], path)
+                data[keyEvent] = path
+            else:
+            # online ver (cors ?)
+                data[keyEvent] = baseUrl + "/" + asset["resource_path"]
+    
+    with open("./data/audio.json", "w") as f:
+        json.dump(data, f, indent=4)
+    print("="*30)
+    print("Done!")
