b0d5b8d3c73236d8bd448f7b4252c499650a8222
Sunset <26019675+lwd-temp@users.noreply.github.com>
2022-09-20 17:45:08 +0800

    skel name should be the same as the folder name

--- py/generateModelsJson.py
+++ py/generateModelsJson.py
@@ -9,8 +9,13 @@ for i in parentDir:
     print(i)
     file = [x for x in (os.listdir(f"assets/spine/{i}")) if ".skel" in x]
     if len(file) > 1:
+        # There should be only one skel in the folder
+        # This happens when the developer packs by mistake
         for j in file:
             if j[0] == "_":
+                continue
+            if j[:-5].lower() not in i.lower():
+                # skel name should be the same as the folder name
                 continue
             data[j[:-5]] = f"assets/spine/{i}/{j}"
     else:
@@ -20,6 +25,11 @@ for i in parentDir:
 
 if not(os.path.isdir("./data")):
     os.mkdir("./data")
+
+# Sort the data by key by alphabet
+data = dict(sorted(data.items()))
+
+print(data)
 
 with open("./data/models.json", "w") as f:
     json.dump(data, f, indent=6, sort_keys=True)
