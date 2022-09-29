222c62bb3ffe0d2f9625fee25a977b0a0a91b60a
Sunset <26019675+lwd-temp@users.noreply.github.com>
2022-09-29 18:33:19 +0800

    There may be more than one skel in the folder, and there's also possibility of pack mistakes.

--- py/generateModelsJson.py
+++ py/generateModelsJson.py
@@ -6,6 +6,22 @@ parentDir = os.listdir("assets/spine/")
 data = {}
 
 for i in parentDir:
+    # There may be more than one skel in the folder, and there's also possibility of pack mistakes.
+    # Currently, not handling this.
+    print(i)
+    file = [x for x in (os.listdir(f"assets/spine/{i}")) if ".skel" in x]
+    if len(file) > 1:
+        for j in file:
+            if j[0] == "_":
+                continue
+            data[j[:-5]] = f"assets/spine/{i}/{j}"
+    else:
+        if i[0] == "_":
+            continue
+        data[i] = f"assets/spine/{i}/" + ''.join(file)
+
+"""
+for i in parentDir:
     print(i)
     file = [x for x in (os.listdir(f"assets/spine/{i}")) if ".skel" in x]
     if len(file) > 1:
@@ -26,14 +42,11 @@ for i in parentDir:
         if i[0] == "_":
             continue
         data[i] = f"assets/spine/{i}/" + ''.join(file)
+"""
 
-if not(os.path.isdir("./data")):
+
+if not (os.path.isdir("./data")):
     os.mkdir("./data")
-
-# Sort the data by key by alphabet
-data = dict(sorted(data.items()))
-
-print(data)
 
 with open("./data/models.json", "w") as f:
     json.dump(data, f, indent=6, sort_keys=True)
