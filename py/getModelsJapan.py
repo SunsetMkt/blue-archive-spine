1cce38836ab4a6ac8db392f43f1824d386ad11c0
Nemo Xiong <xiongnemo@126.com>
2022-12-01 19:21:05 +0800

    update: hardcoded BA(JP) bootstrap endpoint from r49 to r50

--- py/getModelsJapan.py
+++ py/getModelsJapan.py
@@ -13,7 +13,7 @@ option = {
     "skipExistingAssets": True
 }
 
-ba_api = "https://yostar-serverinfo.bluearchiveyostar.com/r49_mkbvd6abx0woazxgpyjr.json"
+ba_api = "https://yostar-serverinfo.bluearchiveyostar.com/r50_d561ioydiypbrigv2h6a.json"
 
 ba_api2 = "https://prod-noticeindex.bluearchiveyostar.com/prod/index.json"
 
